ac22ad9188fb630321849647956e90f83344d72c
Yun-City <94630326+Yun-City@users.noreply.github.com>
2021-11-19 16:12:34 +0800

    财富岛互助

--- gua_wealth_island_help.js
+++ gua_wealth_island_help.js
@@ -80,10 +80,10 @@ $.appId = 10032;
   let getShareNum = 10
   let getShareNums = 0
   if (HelpAuthorFlag) {
-    $.innerInviteList = await getAuthorShareCode('https://raw.githubusercontent.com/sm123iek2221/updateTeam/master/shareCodes/wealth_island_code_one.json');
-    if(!$.innerInviteList[0]) $.innerInviteList = await getAuthorShareCode('https://gitee.com/smi123ek2221/updateTeam/raw/master/shareCodes/wealth_island_code_one.json');
-    res2 = await getAuthorShareCode('https://raw.githubusercontent.com/smie123k2221/updateTeam/master/shareCodes/wealth_island_code.json');
-    if(!res2[0]) res2 = await getAuthorShareCode('https://gitee.com/smi11123ek2221/updateTeam/raw/master/shareCodes/wealth_island_code.json');
+    $.innerInviteList = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/master/shareCodes/wealth_island_code.json');
+    if(!$.innerInviteList[0]) $.innerInviteList = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/master/shareCodes/wealth_island_code.json');
+    res2 = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/master/shareCodes/wealth_island_code.json');
+    if(!res2[0]) res2 = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/master/shareCodes/wealth_island_code.json');
     getShareNums = [...res, ...res2].length >= getShareNum ? getShareNum : [...res, ...res2].length
     $.innerInviteLists = getRandomArrayElements([...res, ...res2], [...res, ...res2].length >= getShareNum ? getShareNum : [...res, ...res2].length );
     $.InviteLists.push(...$.InviteList,...$.innerInviteList,...$.innerInviteLists);
