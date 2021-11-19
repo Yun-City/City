461c059ee9990d4b9eba52522ec1e84df1c3073b
Yun-City <94630326+Yun-City@users.noreply.github.com>
2021-11-19 16:31:53 +0800

    东东世界

--- jd_ddworld.js
+++ jd_ddworld.js
@@ -67,16 +67,16 @@ let tokenInfo = {}, hotInfo = {}
       hotInfo[$.UserName] = $.hot
     }
   }
-  let res = await getAuthorShareCode('https://raw.githubusercontent.com/Aaron-lv/updateTeam/master/shareCodes/ddworld.json')
+  let res = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/main/shareCodes/ddworld.json')
   if (!res) {
-    $.http.get({url: 'https://purge.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/ddworld.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: 'https://raw.githubusercontent.com/Yun-City/City/main/shareCodes/ddworld.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    res = await getAuthorShareCode('https://cdn.jsdelivr.net/gh/Aaron-lv/updateTeam@master/shareCodes/ddworld.json')
+    res = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/main/shareCodes/ddworld.json')
   }
-  let res2 = await getAuthorShareCode('https://raw.githubusercontent.com/zero205/updateTeam/main/shareCodes/ddworld.json')
+  let res2 = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/main/shareCodes/ddworld.json')
   if (!res2) {
     await $.wait(1000)
-    res2 = await getAuthorShareCode('https://raw.fastgit.org/zero205/updateTeam/main/shareCodes/ddworld.json')
+    res2 = await getAuthorShareCode('https://raw.githubusercontent.com/Yun-City/City/main/shareCodes/ddworld.json')
   }
   $.shareCodes = [...$.shareCodes, ...[...(res || []), ...(res2 || [])].sort(() => 0.5 - Math.random())]
   for (let i = 0; i < cookiesArr.length; i++) {
