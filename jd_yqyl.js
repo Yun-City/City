10fcc8902ff1782024598626eed4855d03383e12
Yun-City <94630326+Yun-City@users.noreply.github.com>
2021-11-28 17:51:55 +0800

    更新 jd_yqyl.js

--- jd_yqyl.js
+++ jd_yqyl.js
@@ -18,7 +18,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let yqm = '%2BQCXXvtwyKI2NsJA5uwjUlqanK26xZD05RmobtY3eTA%3D';
+let yqm = '';
 let zdtx = false //设置为true自动抢提现100
 if (process.env.yqm) {
   yqm = process.env.yqm;
