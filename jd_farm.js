8cf539509881a307b64c8a19395c53b61df71c95
Yun-City <94630326+Yun-City@users.noreply.github.com>
2021-12-05 16:54:38 +0800

    更新 jd_farm.js

--- jd_farm.js
+++ jd_farm.js
@@ -1088,7 +1088,6 @@ function shareCodesFormat() {
     if ($.shareCodesArr[$.index - 1]) {
       newShareCodes = $.shareCodesArr[$.index - 1].split('@');
     } else {
-      console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
       newShareCodes = shareCodes[tempIndex].split('@');
     }
