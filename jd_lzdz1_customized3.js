d5fa8ad9761380b79192e757fba4276e28fc6e42
Yun-City <94630326+Yun-City@users.noreply.github.com>
2021-12-10 01:18:55 +0800

    更新 jd_lzdz1_customized3.js

--- jd_lzdz1_customized3.js
+++ jd_lzdz1_customized3.js
@@ -29,11 +29,9 @@ if ($.isNode()) {
         $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
         return;
     }
-    authorCodeList = await getAuthorCodeList('https://gitee.com/fatelight/code/raw/master/lzdz1_customized3.json')
+    authorCodeList = await getAuthorCodeList('https://gitee.com/fatelight/code/raw/master/')
     if($.getAuthorCodeListerr === false){
-        authorCodeList = [
-            'd8625d636ef44b768f425783965fdd69',
-        ]
+        authorCodeList = ['']
     }
     // console.log(authorCodeList)
     for (let i = 0; i < cookiesArr.length; i++) {
