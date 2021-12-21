3e2734866a6693961568974fdaa589ebbc4edf86
Yun-City <94630326+Yun-City@users.noreply.github.com>
2021-12-22 02:55:23 +0800

    ccwav依赖更新

--- GetDeps_PRC.sh
+++ GetDeps_PRC.sh
@@ -1,5 +1,11 @@
-wget https://ghproxy.com/https://raw.githubusercontent.com/ccwav/QLScript2/main/ql.js -O /ql/deps/ql.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/ccwav/QLScript2/main/sendNotify.js -O /ql/deps/sendNotify.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/JS_USER_AGENTS.js -O /ql/deps/JS_USER_AGENTS.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/USER_AGENTS.js -O /ql/deps/USER_AGENTS.js
-wget https://ghproxy.com/https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jdCookie.js -O /ql/deps/jdCookie.js
+#!/usr/bin/env bash
+#
+
+'''
+new Env('ccwav依赖2.10.2以上一键安装');
+'''
+wget https://ghproxy.com/https://raw.githubusercontent.com/Yun-City/City/main/ql.js -O /ql/deps/ql.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Yun-City/City/main/sendNotify.js -O /ql/deps/sendNotify.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Yun-City/City/main/JS_USER_AGENTS.js -O /ql/deps/JS_USER_AGENTS.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Yun-City/City/main/USER_AGENTS.js -O /ql/deps/USER_AGENTS.js
+wget https://ghproxy.com/https://raw.githubusercontent.com/Yun-City/City/main/jdCookie.js -O /ql/deps/jdCookie.js
