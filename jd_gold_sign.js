4cf5e244bd1666d52da8edc92ef99048d8a09e21
Yun-City <94630326+Yun-City@users.noreply.github.com>
2022-01-05 00:29:27 +0800

    京东金榜

--- jd_gold_sign.js
+++ jd_gold_sign.js
@@ -59,6 +59,7 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
         }
         continue
       }
+      await goldCreatorDoTask({ "type": 1 })
       await goldCenterHead();
 
     }
@@ -86,11 +87,9 @@ function goldCenterHead() {
           if (safeGet(data)) {
             data = JSON.parse(data)
             if (data.code === '0') {
-              // console.log(data);
               if (data.result.medalNum === 5) {
+                await $.wait(1500)
                 await goldCreatorDoTask({ "type": 2 })
-              } else {
-                await goldCreatorDoTask({ "type": 1 })
               }
             } else {
               console.log(`失败：${JSON.stringify(data)}\n`);
@@ -109,7 +108,6 @@ function goldCenterHead() {
 function goldCreatorDoTask(body) {
   return new Promise(resolve => {
     const options = taskUrl('goldCenterDoTask', body)
-    // console.log(options);
     $.get(options, async (err, resp, data) => {
       try {
         if (err) {
