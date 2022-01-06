6e0180df9b4a78611ba72c46abe503a2a6db9e2e
Yun-City <94630326+Yun-City@users.noreply.github.com>
2022-01-06 15:02:15 +0800

    更新 jd_wish.js

--- jd_wish.js
+++ jd_wish.js
@@ -25,8 +25,8 @@ let message = '', allMessage = '';
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
-let appIdArr = ['1E1xZy6s','1FFVQyqw','1GVFUx6g'];
-let appNameArr = ['PLUS生活特权','1111点心动','joy的年味之旅'];
+let appIdArr = ['1E1xZy6s','1FFVQyqw','1GVFUx6g','1GVJWyqg'];
+let appNameArr = ['PLUS生活特权','1111点心动','joy的年味之旅','虎娃迎福'];
 let appId, appName;
 $.shareCode = [];
 if ($.isNode()) {
