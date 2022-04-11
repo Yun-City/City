2b69f334f9a82fc6129ebaa1517fdd21074bdff4
Gitea <94630326+Yun-City@users.noreply.github.com>
2022-04-12 00:43:21 +0800

    删除

--- jd_carnivalcity.js
+++ jd_carnivalcity.js
@@ -45,7 +45,6 @@ let nowTime = new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 
       if ($.isNode()) await notify.sendNotify($.name + '活动已结束', `请删除此脚本\n咱江湖再见`);
       return
     }
-    await updateShareCodesCDN();
 
     for (let i = 0; i < cookiesArr.length; i++) {
       if (cookiesArr[i]) {
@@ -733,29 +732,6 @@ function getListRank() {
   })
 }
 
-function updateShareCodesCDN() {
-  return new Promise(resolve => {
-    $.get({
-      url: "https://api.jdsharecode.xyz/api/carnivalcity/30",
-      headers: {"User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")},
-      timeout: 10000
-    }, async (err, resp, data) => {
-      try {
-        if (err) {
-          console.log(`${JSON.stringify(err)}`)
-          console.log(`${$.name} API请求失败，请检查网路重试`)
-        } else {
-          data = JSON.parse(data)
-          $.updatePkActivityIdRes = data.data;
-        }
-      } catch (e) {
-        $.logErr(e, resp)
-      } finally {
-        resolve();
-      }
-    })
-  })
-}
 
 function taskPostUrl(a, t = {}) {
   const body = $.toStr({...t, "apiMapping": `${a}`})
