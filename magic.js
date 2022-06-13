b54a4e1d5b4cde7620c8f940b49a0dc71e0251c8
Gitea <94630326+Yun-City@users.noreply.github.com>
2022-06-13 16:56:22 +0800

    更新 magic.js

--- magic.js
+++ magic.js
@@ -3,12 +3,13 @@
 const axios = require('axios');
 const fs = require("fs");
 const {format} = require("date-fns");
-const notify = require('./sendNotify');
-const jdCookieNode = require('./jdCookie.js');
+const notify = require('../sendNotify');
+const jdCookieNode = require('../jdCookie.js');
 const CryptoJS = require("crypto-js");
+const got = require("got");
 let cookies = [];
 let testMode = process.env.TEST_MODE?.includes('on') ? true
-    : __dirname.includes("/home/magic")
+    : __dirname.includes("magic")
 Object.keys(jdCookieNode).forEach((item) => {
     cookies.push(jdCookieNode[item])
 })
@@ -52,14 +53,13 @@ const USER_AGENTS = [
     "jdapp;iPhone;10.0.2;14.1;network/wifi;Mozilla/5.0 (iPhone; CPU iPhone OS 14_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1",
 ]
 
-const $ = axios.create({timeout: 4000});
+const $ = axios.create({timeout: 24000});
 $.defaults.headers['Accept'] = '*/*';
 $.defaults.headers['User-Agent'] = USER_AGENTS[randomNumber(0,
     USER_AGENTS.length)];
 $.defaults.headers['Connection'] = 'keep-alive';
 $.defaults.headers['Accept-Language'] = "zh-CN,zh-Hans;q=0.9";
 $.defaults.headers['Accept-Encoding'] = "gzip, deflate, br";
-
 
 
 function randomNumber(min = 0, max = 100) {
@@ -87,23 +87,18 @@ class Env {
         wait: [1000, 2000],
         bot: false,
         delimiter: '',
-        filename: '',
         o2o: false,
         random: false,
         once: false,
         blacklist: [],
         whitelist: []
     }) {
-        console.log(
-            `${this.now()} ${this.name} ${data?.filename ? data?.filename
-                : ''} 开始运行...`);
+        this.filename = process.argv[1];
+        console.log(`${this.now()} ${this.name} ${this.filename} 开始运行...`);
         this.start = this.timestamp();
         await this.config()
         if (data?.delimiter) {
             this.delimiter = data?.delimiter
-        }
-        if (data?.filename) {
-            this.filename = data.filename;
         }
         if (data?.bot) {
             this.bot = data.bot;
@@ -171,7 +166,7 @@ class Env {
                     await this.logic()
                     if (data?.o2o) {
                         await this.send();
-                        testMode ? this.log(this.msg) : ''
+                        testMode ? this.log(this.msg.join("\n")) : ''
                         this.msg = [];
                     }
                     if (once) {
@@ -188,7 +183,7 @@ class Env {
         await this.after()
         console.log(`${this.now()} ${this.name} 运行结束,耗时 ${this.timestamp()
         - this.start}ms\n`)
-        testMode && this.msg.length > 0 ? console.log(this.msg) : ''
+        testMode && this.msg.length > 0 ? console.log(this.msg.join("\n")) : ''
         if (!data?.o2o) {
             await this.send();
         }
@@ -253,13 +248,11 @@ class Env {
             === String.fromCharCode(77)
                 ? (fn.includes(av(y)) ? '10032' :
                     fn.includes(av(z)) ? '10028' :
-                        fn.includes(av(j)) ? 'c0ff1' :
+                        fn.includes(av(j)) ? '10001' :
                             fn.includes(av(k)) ? '10038' :
                                 fn.includes(av(m)) ? 'wx' : '') : ''
             : '';
-        if (this.appId !== 'wx') {
-            this.appId ? this.algo = await this._algo() : '';
-        }
+        this.appId ? this.algo = await this._algo() : '';
     }
 
     async wait(min, max) {
@@ -273,8 +266,23 @@ class Env {
 
     putMsg(msg) {
         this.log(msg)
-        this.bot ? this.msg.push(msg) :
-            this.msg.push(`【当前账号】 ${this.username} ${msg}`)
+        let r = [[' ', ''], ['优惠券', '券'], ['东券', '券'], ['店铺', ''],
+            ['恭喜', ''], ['获得', '']]
+        for (let ele of r) {
+            msg = msg.replace(ele[0], ele[1])
+        }
+        if (this.bot) {
+            this.msg.push(msg)
+        } else {
+            if (this.msg.length > 0 && this.msg[this.msg.length - 1].includes(
+                this.username)) {
+                this.msg[this.msg.length - 1] = this.msg[this.msg.length
+                - 1].split(" ")[0] + ' ' + [this.msg[this.msg.length - 1].split(
+                    " ")[1], msg].join(',')
+            } else {
+                this.msg.push(`【当前账号】${this.username} ${msg}`)
+            }
+        }
     }
 
     md5(str) {
@@ -288,6 +296,21 @@ class Env {
     log(...msg) {
         this.s ? console.log(...msg) : console.log(
             `${this.now()} ${this.username}`, ...msg)
+    }
+
+    //并
+    union(a, b) {
+        return a.concat(b.filter(o => !a.includes(o)))
+    }
+
+    //交
+    intersection(a, b) {
+        return a.filter(o => b.includes(o))
+    }
+
+    //交
+    different(a, b) {
+        return a.concat(b).filter(o => a.includes(o) && !b.includes(o))
     }
 
     build(url) {
@@ -426,6 +449,29 @@ class Env {
         return '';
     }
 
+    matchAll(pattern, string) {
+        pattern = (pattern instanceof Array) ? pattern : [pattern];
+        let match;
+        let result = [];
+        for (let p of pattern) {
+            while ((match = p.exec(string)) != null) {
+                let len = match.length;
+                if (len === 1) {
+                    result.push(match);
+                } else if (len === 2) {
+                    result.push(match[1]);
+                } else {
+                    let r = [];
+                    for (let i = 1; i < len; i++) {
+                        r.push(match[i])
+                    }
+                    result.push(r);
+                }
+            }
+        }
+        return result;
+    }
+
     async countdown(s) {
         let date = new Date();
         if (date.getMinutes() === 59) {
@@ -486,24 +532,29 @@ class Env {
         })
     }
 
+    //└
     async request(url, headers, body) {
         return new Promise((resolve, reject) => {
-            (body ? $.post(url, body, {headers: headers}) : $.get(url,
-                {headers: headers}))
+            let __config = headers?.headers ? headers : {headers: headers};
+            (body ? $.post(url, body, __config) : $.get(url, __config))
             .then(data => {
-                this.__lt(data.headers);
+                this.__lt(data);
                 resolve(data)
             })
             .catch(e => reject(e));
         })
     }
 
-    __lt(headers) {
+    __lt(data) {
         if (this.appId.length !== 2) {
             return
         }
-        let scs = headers['set-cookie'] || headers['Set-Cookie'] || ''
+        let scs = data?.headers['set-cookie'] || data?.headers['Set-Cookie']
+            || ''
         if (!scs) {
+            if (data?.data?.LZ_TOKEN_KEY && data?.data?.LZ_TOKEN_VALUE) {
+                this.lz = `LZ_TOKEN_KEY=${data.data.LZ_TOKEN_KEY};LZ_TOKEN_VALUE=${data.data.LZ_TOKEN_VALUE};`;
+            }
             return;
         }
         let LZ_TOKEN_KEY = '', LZ_TOKEN_VALUE = ''
@@ -520,11 +571,11 @@ class Env {
         if (LZ_TOKEN_KEY && LZ_TOKEN_VALUE) {
             this.lz = `${LZ_TOKEN_KEY}${LZ_TOKEN_VALUE}`
         }
-        testMode ? this.log('lz', this.lz) : ''
+        // testMode ? this.log('lz', this.lz) : ''
     }
 
     handler(res) {
-        let data = res.data;
+        let data = res?.data || res?.body ||res;
         if (!data) {
             return;
         }
@@ -537,9 +588,11 @@ class Env {
                 let st = data.replace(/[\n\r]/g, '').replace(/jsonpCB.*\({/,
                     '{');
                 data = st.substring(0, st.length - 1)
-            } else if (/try{.*\({/) {
+            } else if (data.match(/try{.*\({/)) {
                 data = data.replace(/try{.*\({/, '{')
                 .replace(/}\)([;])?}catch\(e\){}/, '}')
+            } else if (data.includes("jsonp")) {
+                data = /{(.*)}/g.exec(data)[0]
             } else {
                 testMode ? console.log('例外', data) : ''
             }
@@ -571,6 +624,24 @@ class Env {
             const r = 16 * Math.random() | 0, n = "x" === x ? r : 3 & r | 8;
             return n.toString(36)
         })
+    }
+
+    async unfollow(shopId) {
+        let url = 'https://api.m.jd.com/client.action?g_ty=ls&g_tk=518274330'
+        let body = `functionId=followShop&body={"follow":"false","shopId":"${shopId}","award":"true","sourceRpc":"shop_app_home_follow"}&osVersion=13.7&appid=wh5&clientVersion=9.2.0&loginType=2&loginWQBiz=interact`
+        let headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Host': 'api.m.jd.com',
+            'Connection': 'keep-alive',
+            'Accept-Language': 'zh-cn',
+            'Cookie': this.cookie
+        }
+        headers['User-Agent'] = `Mozilla/5.0 (iPhone; CPU iPhone OS 14_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.4(0x1800042c) NetType/4G Language/zh_CN miniProgram`
+        let {data} = await this.request(url, headers, body);
+        this.log(data.msg)
+        return data;
     }
 
     randomCallback(e = 1) {
@@ -623,7 +694,7 @@ class Env {
     }
 
     async get_bean() {
-        let data = await $.post('https://api.m.jd.com/client.action',
+        let {data} = await $.post('https://api.m.jd.com/client.action',
             `functionId=plantBeanIndex&body=${escape(
                 JSON.stringify({
                     version: "9.0.0.1",
@@ -635,11 +706,12 @@ class Env {
                 'Host': "api.m.jd.com",
                 "Cookie": this.cookie
             });
+        debugger
         return data.data.jwordShareInfo.shareUrl.split('Uuid=')[1] ?? ''
     }
 
     async get_farm() {
-        let data = await $.post(
+        let {data} = await $.post(
             'https://api.m.jd.com/client.action?functionId=initForFarm',
             `body=${escape(
                 JSON.stringify({"version": 4}))}&appid=wh5&clientVersion=9.1.0`,
@@ -649,17 +721,8 @@ class Env {
                 "Content-Type": "application/x-www-form-urlencoded",
                 "Cookie": this.cookie
             })
+        debugger
         return data?.farmUserPro?.shareCode ?? ''
-    }
-
-    async sign(fn, body = {}) {
-        const got = require('got');
-        const data = await got.post('https://api.jds.codes/sign', {
-            json: {"fn": fn, "body": body},
-            responseType: 'json'
-        });
-        testMode ? JSON.stringify(data.body) : '';
-        return data.body.data;
     }
 
     async _algo() {
